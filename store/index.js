1661a7a579ef23d0be769080ff241d0c0ccbdaf9
ElizavetaGorelova <e.gorelova@tg.dunice.net>
2022-10-04 17:36:38 +0300

    add navbar

---
+++ store/index.js
@@ -0,0 +1,21 @@
+export const state = () => ({
+    bitcoinRate: 1000
+})
+
+export const mutations = () => ({
+    setBitcoinRate(state) {
+        
+    }
+})
+ 
+export const getters = {
+getBitcoinRate(store) {
+    return store.bitcoinRate
+}
+}
+
+export const actions = {
+    changeBitcoinRate() {
+
+    }
+}
